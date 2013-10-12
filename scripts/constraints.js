bf83aa857cdce1e7e6f5ef4ce28601ddb626e083
James Baicoianu <james_github@baicoianu.com>
2013-10-12 04:08:29 -0700

    Imported initial constraints file

---
+++ scripts/constraints.js
@@ -0,0 +1,12 @@
+/**
+ * constraints
+ */
+
+// pivot rotates a single body around its origin, with angular constraints
+elation.extend("physics.constraints.pivot", function(body, args) {
+});
+
+// hinge mounts to one body, and pivots another
+elation.extend("physics.constraints.hinge", function(body, args) {
+});
+
