d30ad59b2eab254fb0d71e89549f9dac9b988516
James Baicoianu <james_github@baicoianu.com>
2013-10-12 04:05:30 -0700

    Collision coordinate space and handling issues partly resolved

--- scripts/collisions.js
+++ scripts/collisions.js
@@ -4,38 +4,43 @@
 
 elation.extend("physics.colliders.helperfuncs", new function() {
   this._tmpvec = new THREE.Vector3();
+  var _tmpvec2 = new THREE.Vector3();
+  var _tmpvec3 = new THREE.Vector3();
 
   this.sphere_sphere = function(obj1, obj2, collisions) {
     var contact = false;
-    var thispos = obj1.body.object.matrixWorld.getPosition().clone(),
-        otherpos = obj2.body.object.matrixWorld.getPosition().clone(),
-        midline = this._tmpvec.sub(thispos, otherpos),
+    var thispos = obj1.body.localToWorldPos(_tmpvec2.set(0,0,0)),
+        otherpos = obj2.body.localToWorldPos(_tmpvec3.set(0,0,0)),
+        midline = this._tmpvec.subVectors(otherpos, thispos),
         size = midline.length();
-    if (!(size <= 0 || size > obj1.body.radius + obj2.body.radius)) {
-      collisions.push(new elation.physics.contact({
-        normal: midline.clone().divideScalar(size),
-        point: thispos.clone().addSelf(midline).multiplyScalar(.5),
-        penetration: obj1.body.radius + obj2.body.radius - size,
+    midline.divideScalar(size);
+    if (!(size <= 0 || size >= obj1.radius + obj2.radius)) {
+      contact = new elation.physics.contact({
+        normal: midline.clone(),
+        point: thispos.clone().add(midline.multiplyScalar(obj1.radius)),
+        penetration: obj1.radius + obj2.radius - size,
         object1: obj1.body,
-        object2: obj2.body
-      }));
+        object2: obj2.body,
+      });
+      collisions.push(contact);
     }
     return contact;
   }
   this.sphere_plane = function(sphere, plane, collisions) {
     var contact = false;
-    var pos = this._tmpvec.copy(sphere.body.object.matrixWorld.getPosition());
-    var distance = plane.normal.dot(pos) - sphere.body.radius - plane.offset;
-    //console.log('crash a sphere-plane!', sphere, plane, distance);
-    if (distance <= 0) {
+    var pos = sphere.body.localToWorldPos(this._tmpvec.set(0,0,0));
+    var norm = plane.body.localToWorldDir(plane.normal.clone());
+    var distance = norm.dot(pos) - sphere.radius - plane.offset;
+    if (distance < 0) {
       var sepspeed = sphere.body.velocity.dot(plane.normal);
       if (sepspeed <= 0) {
+        //console.log('crash a sphere-plane!', sphere, plane, distance);
         collisions.push(new elation.physics.contact({
-          normal: plane.normal,
-          point: pos.clone().subSelf(plane.normal.clone().multiplyScalar(distance + sphere.body.radius)), 
+          normal: norm,
+          point: pos.clone().sub(norm.clone().multiplyScalar(distance + sphere.radius)), 
           penetration: -distance,
           object1: sphere.body,
-          //object2: plane.body
+          object2: plane.body,
         }));
       }
     }
@@ -59,9 +64,8 @@ elation.extend("physics.colliders.helperfuncs", new function() {
     ];
     var max = 0;
     for (var i = 0; i < vertices.length; i++) {
-      this._tmpvec.set(vertices[i][0], vertices[i][1], vertices[i][2]);
-      box.body.object.matrixWorld.multiplyVector3(this._tmpvec);
-      var vcollide = this.vertex_plane(this._tmpvec, plane);
+      var worldpos = box.body.localToWorldPos(this._tmpvec.set(vertices[i][0], vertices[i][1], vertices[i][2]));
+      var vcollide = this.vertex_plane(worldpos, plane);
       if (vcollide) {
         vcollide.object1 = box.body;
         contacts.push(vcollide);
@@ -84,7 +88,7 @@ elation.extend("physics.colliders.helperfuncs", new function() {
     if (distance <= plane.offset) {
       contact = {
         normal: plane.normal.clone(),
-        point: plane.normal.clone().multiplyScalar((plane.offset - distance) / 2).addSelf(vertex),
+        point: plane.normal.clone().multiplyScalar((plane.offset - distance) / 2).add(vertex),
         penetration: plane.offset - distance
       };
     }
@@ -94,7 +98,7 @@ elation.extend("physics.colliders.helperfuncs", new function() {
 elation.extend("physics.colliders.sphere", function(body, args) {
   this.type = 'sphere';
   this.body = body;
-  this.body.radius = args;
+  this.radius = args.radius || args;
   this._tmpvec = new THREE.Vector3();
 
   this.getContact = function(other, collisions) {
@@ -104,7 +108,7 @@ elation.extend("physics.colliders.sphere", function(body, args) {
         contact = elation.physics.colliders.helperfuncs.sphere_sphere(this, other, collisions);
         break;
       case 'plane':
-        contact = elation.physics.colliders.helperfuncs.sphere_plane(this, othe, collisionsr);
+        contact = elation.physics.colliders.helperfuncs.sphere_plane(this, other, collisions);
         break;
       default:
         console.log("Error: can't handle " + this.type + "-" + other.type + " collisions yet!");
@@ -118,6 +122,7 @@ elation.extend("physics.colliders.plane", function(body, args) {
   this.body = body;
   this.normal = args.normal || new THREE.Vector3(0,1,0);
   this.offset = args.offset || 0;
+  this.radius = Infinity;
 
   this.getContact = function(other, collisions) {
     var contact = false;
@@ -138,6 +143,7 @@ elation.extend("physics.colliders.box", function(body, args) {
   this.body = body;
   this.min = args.min || new THREE.Vector3(0,0,0);
   this.max = args.max || new THREE.Vector3(0,0,0);
+  this.radius = this.max.clone().sub(this.min).length() / 2;
 
   this.getContact = function(other, collisions) {
     var contact = false;
@@ -159,62 +165,46 @@ elation.extend("physics.contact", function(contactargs) {
   this.normal = contactargs.normal || new THREE.Vector3(0,1,0);
   this.relativePositions = [];
   this.inertialMoments = [];
+  this.impulses = [];
+
+  this._tmpmat = new THREE.Matrix4();
 
   if (contactargs.object1) this.bodies.push(contactargs.object1);
   if (contactargs.object2) this.bodies.push(contactargs.object2);
 
   this.calculateContactMatrix = function() {
-    // FIXME - potential coordinate space confusion
-    var tangents = [];
-    /*
-    if (Math.abs(this.normal.x) > Math.abs(this.normal.y)) {
-      var s = 1 / Math.sqrt(this.normal.z * this.normal.z + this.normal.x * this.normal.x);
-      tangents[0] = { x: this.normal.z * s,
-                      y: 0,
-                      z: -this.normal.x * s
-                    };
-      tangents[1] = { x: this.normal.y * tangents[0].x,
-                      y: this.normal.z * tangents[0].x - this.normal.x * tangents[0].z, 
-                      z: -this.normal.y * tangents[0].x
-                    };
-    } else {
-      var s = 1 / Math.sqrt(this.normal.z * this.normal.z + this.normal.y + this.normal.y);
-      tangents[0] = { x: 0,
-                      y: -this.normal.z * s,
-                      z: this.normal.y * s
-                    };
-      tangents[1] = { x: this.normal.y * tangents[0].z - this.normal.z * tangents[0].y,
-                      y: -this.normal.x * tangents[0].z, 
-                      z: this.normal.x * tangents[0].y
-                    };
-    }
-    */
-    tangents[0] = {x: 0, y: 0, z: 1};
-    tangents[1] = {x: 1, y: 0, z: 0};
-    this.contactToWorld = new THREE.Matrix3();
-    this.contactToWorld.m = [
-      tangents[0].x, tangents[0].y, tangents[0].z,
-      this.normal.x, this.normal.y, this.normal.z,
-      tangents[1].x, tangents[1].y, tangents[1].z];
+    // TODO - make this more memory efficient
 
-    this.worldToContact = new THREE.Matrix3();
-    this.worldToContact.m = [
-      tangents[0].x, this.normal.x, tangents[1].x,
-      tangents[0].y, this.normal.y, tangents[1].y,
-      tangents[0].z, this.normal.z, tangents[1].z
-    ];
+    // Check whether the Z-axis is nearer to the X or Y axis
+    this.normal.normalize();
+    var c1 = this.normal.clone().cross(new THREE.Vector3(0,0,1));
+    var c2 = this.normal.clone().cross(new THREE.Vector3(0,1,0));
+    var tangent = (c1.lengthSq() > c2.lengthSq() ? c1 : c2);
+    tangent.normalize();
+    var binormal = this.normal.clone().cross(tangent).normalize();
+
+    this.contactToWorld = new THREE.Matrix4(
+      tangent.x, this.normal.x, -binormal.x, 0,
+      tangent.y, this.normal.y, -binormal.y, 0,
+      tangent.z, this.normal.z, -binormal.z, 0,
+      0, 0, 0, 1
+    );
+    this.worldToContact = new THREE.Matrix4().getInverse(this.contactToWorld);
+
     //console.log(MATDUMP(this.contactToWorld.m), this.restitution);
   }
   this.calculateLocalVelocity = function(index, duration) {
     var body = this.bodies[index];
-    var velocity = body.angular.clone().crossSelf(this.relativePositions[index]).addSelf(body.velocity);
+    var velocity = body.angular.clone().cross(this.relativePositions[index]).clone().add(body.velocity);
     var foop = VECDUMP(velocity);
-    this.worldToContact.multiplyVector3(velocity);
+    //this.worldToContact.multiplyVector3(velocity);
+    velocity.applyMatrix4(this.worldToContact);
 
     var accvel = body.lastacceleration.clone().multiplyScalar(duration);
-    this.worldToContact.multiplyVector3(accvel);
+    //this.worldToContact.multiplyVector3(accvel);
+    accvel.applyMatrix4(this.worldToContact);
     //accvel.y = 0;
-    velocity.addSelf(accvel);
+    velocity.add(accvel);
     
     //console.log('localvel was ' + foop + ", now " + VECDUMP(velocity));
     return velocity;
@@ -231,15 +221,17 @@ elation.extend("physics.contact", function(contactargs) {
     this.calculateContactMatrix();
 
     // Calculate relative position and velocity for the first body
-    this.relativePositions[0] = this.point.clone().subSelf(this.bodies[0].getWorldPosition());
+    this.relativePositions[0] = this.point.clone().sub(this.bodies[0].localToWorldPos());
     this.velocity = this.calculateLocalVelocity(0, duration);
-    this.inertialMoments[0] = this.bodies[0].momentInverse.clone().multiplySelf(this.bodies[0].object.matrixWorld);
+    var mworld0 = this._tmpmat.makeRotationFromQuaternion(this.bodies[0].orientationWorld);
+    this.inertialMoments[0] = this.bodies[0].momentInverse.clone().multiply(mworld0);
 
     // If we have a second body, figure out its position and subtract its velocity
     if (this.bodies[1]) {
-      this.relativePositions[1] = this.point.clone().subSelf(this.bodies[1].getWorldPosition());
-      this.velocity.subSelf(this.calculateLocalVelocity(1, duration));
-      this.inertialMoments[1] = this.bodies[1].momentInverse.clone().multiplySelf(this.bodies[1].object.matrixWorld);
+      this.relativePositions[1] = this.point.clone().sub(this.bodies[1].localToWorldPos());
+      this.velocity.sub(this.calculateLocalVelocity(1, duration));
+      var mworld1 = this._tmpmat.makeRotationFromQuaternion(this.bodies[1].orientationWorld);
+      this.inertialMoments[1] = this.bodies[1].momentInverse.clone().multiply(mworld1);
     }
     this.calculateDesiredDeltaVelocity(duration);
   }
@@ -247,15 +239,38 @@ elation.extend("physics.contact", function(contactargs) {
     // determine new velocities
 
     var impulse = this.calculateFrictionlessImpulse();
-    this.contactToWorld.multiplyVector3(impulse);
+//console.log('apply impulse', impulse.toArray(), this);
+    //this.contactToWorld.multiplyVector3(impulse);
+    impulse.applyMatrix4(this.contactToWorld);
+    this.impulse = impulse;
 
+/*
     for (var i = 0; i < this.bodies.length; i++) {
       var body = this.bodies[i];
-      // FIXME - cyclone swaps impulse and relativeposition for the second object.  which is correct?
-      var impulsiveTorque = this.relativePositions[i].clone().crossSelf(impulse);
-      body.addVelocity(impulse.multiplyScalar(1/body.mass));
-      //body.addAngularVelocity(this.inertialMoments[i].multiplyVector3(impulsiveTorque));
+      if (body.mass > 0) {
+        // FIXME - cyclone swaps impulse and relativeposition for the second object.  which is correct?
+        //var impulsiveTorque = this.relativePositions[i].clone().cross(impulse);
+  //body.velocity.set(0,0,0);
+        this.impulses[i] = impulse.clone().multiplyScalar(1/body.mass * (i == 0 ? 1 : -1));
+        body.addVelocity(this.impulses[i]);
+        //body.addAngularVelocity(this.inertialMoments[i].multiplyVector3(impulsiveTorque));
+        //body.addAngularVelocity(impulsiveTorque.applyMatrix4(this.inertialMoments[i]));
+      }
     }
+*/
+
+    if (this.bodies[0] && this.bodies[0].mass > 0) {
+      //var impulsiveTorque = this.relativePositions[0].clone().cross(impulse);
+      this.impulses[0] = impulse.clone().multiplyScalar(1 / this.bodies[0].mass);
+      this.bodies[0].addVelocity(this.impulses[0]);
+    }
+
+    if (this.bodies[1] && this.bodies[1].mass > 0) {
+      //var impulsiveTorque = impulse.clone().cross(this.relativePositions[1]);
+      this.impulses[1] = impulse.clone().multiplyScalar(-1 / this.bodies[1].mass);
+      this.bodies[1].addVelocity(this.impulses[1]);
+    }
+
     //console.log(VECDUMP(impulse), VECDUMP(impulsiveTorque), VECDUMP(this.bodies[0].velocity));
     //this.bodies[0].velocity.set(0,0,0);
     //this.bodies[0].angular.set(0,0,0);
@@ -266,10 +281,13 @@ elation.extend("physics.contact", function(contactargs) {
     
     var deltaVelocity = 0;
     for (var i = 0; i < this.bodies.length; i++) {
-      var deltaVelWorld = this.relativePositions[i].clone().crossSelf(this.normal);
-      this.inertialMoments[i].multiplyVector3(deltaVelWorld);
-      deltaVelWorld.crossSelf(this.relativePositions[i]);
-      deltaVelocity += deltaVelWorld.dot(this.normal) + 1 / this.bodies[i].mass;
+      if (this.bodies[i].mass > 0) {
+        var deltaVelWorld = this.relativePositions[i].clone().cross(this.normal);
+        //this.inertialMoments[i].multiplyVector3(deltaVelWorld);
+        deltaVelWorld.applyMatrix4(this.inertialMoments[i]);
+        deltaVelWorld.cross(this.relativePositions[i]);
+        deltaVelocity += deltaVelWorld.dot(this.normal) + 1 / this.bodies[i].mass;
+      }
     }
     
     // FIXME - potential coordinate space confusion
@@ -289,38 +307,48 @@ elation.extend("physics.contact", function(contactargs) {
     var linearChange = [],
         angularChange = [];
     
+    var totalMass = 0;
     for (var i = 0; i < this.bodies.length; i++) {
       var body = this.bodies[i];
-      var angularinertiaworld = this.relativePositions[i].clone().crossSelf(this.normal);
-      this.inertialMoments[i].multiplyVector3(angularinertiaworld);
-      angularinertiaworld.crossSelf(this.relativePositions[i]);
+      if (body) {
+        var angularInertiaWorld = this.relativePositions[i].clone().cross(this.normal);
+        angularInertiaWorld.applyMatrix4(this.inertialMoments[i]);
+        angularInertiaWorld.cross(this.relativePositions[i]);
+        angularInertia[i] = 0;//angularInertiaWorld.dot(this.normal);
 
-      angularInertia[i] = angularinertiaworld.dot(this.normal);
-      linearInertia[i] = 1 / body.mass;
-      totalInertia += linearInertia[i] + angularInertia[i];
-    }
-
-    for (var i = 0; i < this.bodies.length; i++) {
-      var sign = (i == 0 ? 1 : -1);
-      angularMove[i] = sign * this.penetration * (angularInertia[i] / totalInertia);
-      linearMove[i] = sign * this.penetration;// * (linearInertia[i] / totalInertia);
-
-      // TODO - fancy stuff with maxMagnitude
-
-      if (angularMove[i] != 0) {
-        var targetAngularDirection = this.relativePositions[i].clone().multiplySelf(this.normal);
-        this.inertialMoments[i].multiplyVector3(targetAngularDirection);
-        angularChange[i] = targetAngularDirection.multiplyScalar(angularMove[i] / angularInertia[i]);
+        linearInertia[i] = (body.mass == 0 ? 0 : 1 / body.mass);
+        totalInertia += linearInertia[i] + angularInertia[i];
+        totalMass += body.mass;
       }
-      linearChange[i] = this.normal.clone().multiplyScalar(linearMove[i]);
-      this.bodies[i].position.addSelf(linearChange[i]);
-
-      var q = new THREE.Quaternion();
-      var theta = angularChange[i].length();
-      // FIXME - potential coordinate space confusion
-      q.setFromAxisAngle(angularChange[i].clone().divideScalar(theta), theta);
-      //this.bodies[i].orientation.copy(q);
     }
+
+    if (totalMass > 0) { // Unstoppable force, immovable object
+      for (var i = 0; i < this.bodies.length; i++) {
+        var sign = (i == 0 ? 1 : -1);
+        angularMove[i] = sign * this.penetration * (angularInertia[i] / totalInertia);
+        linearMove[i] = sign * this.penetration * (linearInertia[i] / totalInertia);
+
+        // TODO - fancy stuff with maxMagnitude to prevent uncontrolled spinning
+
+/*
+        if (angularMove[i] != 0) {
+          var targetAngularDirection = this.relativePositions[i].clone().multiply(this.normal);
+          targetAngularDirection.applyMatrix4(this.inertialMoments[i]);
+          angularChange[i] = targetAngularDirection.multiplyScalar(angularMove[i] / angularInertia[i]);
+        }
+*/
+        linearChange[i] = this.normal.clone().multiplyScalar(linearMove[i]);
+        this.impulses[i] = linearChange[i];
+        this.bodies[i].position.add(linearChange[i]);
+
+        //var q = new THREE.Quaternion();
+        //var theta = angularChange[i].length();
+        // FIXME - potential coordinate space confusion
+        //q.setFromAxisAngle(angularChange[i].clone().divideScalar(theta), theta);
+        //this.bodies[i].orientation.copy(q);
+      }
+    }
+    //console.log('move it', linearChange, linearMove, linearInertia, angularInertia, totalInertia, this);
   }
 });
 
