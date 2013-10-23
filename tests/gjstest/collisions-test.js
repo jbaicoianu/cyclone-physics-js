515d136e7a1c8961af8ebe41b4ba7e628704da4d
James Baicoianu <james_github@baicoianu.com>
2013-10-23 12:16:50 -0700

    Added sphere-sphere collision test

--- tests/gjstest/collisions-test.js
+++ tests/gjstest/collisions-test.js
@@ -4,3 +4,30 @@ function CollisionsTest() {
 }
 registerTestSuite(CollisionsTest);
 
+CollisionsTest.prototype.testSphereSphere = function() {
+  var body1 = new elation.physics.rigidbody({mass: 1, position: new THREE.Vector3(-10,0,0), velocity: new THREE.Vector3(1,0,0)});
+  body1.setCollider('sphere', 1);
+
+  var body2 = new elation.physics.rigidbody({mass: 1, position: new THREE.Vector3(10,0,0), velocity: new THREE.Vector3(-1,0,0)});
+  body2.setCollider('sphere', 1);
+
+  elation.physics.system.add(body1);
+  elation.physics.system.add(body2);
+
+  var state = {
+    collisions: []
+  };
+  elation.events.add(body1, 'physics_collide', function(ev) { state.collisions.push(ev.data); });
+  elation.events.add(body2, 'physics_collide', function(ev) { state.collisions.push(ev.data); });
+
+  for (var i = 0; i < this.numsteps; i++) {
+    elation.physics.system.step(this.stepsize);
+    if (state.collisions.length >= 2) {
+      console.log('crash!');
+      state.collisions = [];
+      //break;
+    }
+  }
+  
+}
+
