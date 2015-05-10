2789ced6031832ce08b896ca50ee0b1fdb713455
James Baicoianu <james_github@baicoianu.com>
2015-05-10 12:42:23 -0700

    Added constraint support to rigidbody

--- scripts/rigidbody.js
+++ scripts/rigidbody.js
@@ -8,6 +8,7 @@ elation.extend("physics.rigidbody", function(args) {
   this.angular = new THREE.Vector3();
   this.angularacceleration = new THREE.Vector3();
   this.forces = [];
+  this.constraints = [];
   this.mass = 0;
   this.state = {sleeping: true, accelerating: false, moving: false, rotating: false};
   this.momentInverse = new THREE.Matrix4().identity();
@@ -36,6 +37,8 @@ elation.extend("physics.rigidbody", function(args) {
   }
   this.updateState = function() {
     var lambda = 0.00001;
+    this.processConstraints();
+
     if (this.parent) {
       this.orientationWorld.multiplyQuaternions(this.parent.orientationWorld, this.orientation);
       this.positionWorld.copy(this.position).applyQuaternion(this.parent.orientation.clone().inverse()).add(this.parent.positionWorld);
@@ -146,6 +149,40 @@ elation.extend("physics.rigidbody", function(args) {
     }
     */
   }
+  this.addConstraint = function(type, args) {
+    var constraint = false;
+    if (typeof elation.physics.constraints[type] == 'function') {
+      constraint = new elation.physics.constraints[type](this, args);
+      this.constraints.push(constraint);
+      //this.updateConstraints();
+      this.updateState();
+      //console.log('added new constraint', constraint);
+    } else {
+      console.log('Unknown constraint type: ' + type);
+    }
+    return constraint;
+  }
+  this.removeConstraint = function(constraint) {
+    var removes = [];
+    if (typeof constraint == 'string') {
+      for (var i = 0; i < this.constraints.length; i++) {
+        if (this.constraints[i] instanceof elation.physics.constraints[constraint]) {
+          removes.unshift(i);
+        }
+      }
+    } else {
+      var idx = this.constraints.indexOf(constraint);
+      if (idx > -1) {
+        removes.push(idx);
+      }
+    }
+    if (removes.length > 0) {
+      removes.sort();
+      for (var i = removes.length; i > 0; --i) {
+        this.constraints.splice(i, 1);
+      }
+    }
+  }
   /*
   this.updateMoment = function(shape, shapeargs) {
     switch (shape) {
@@ -180,6 +217,9 @@ elation.extend("physics.rigidbody", function(args) {
           0, xy, 0, 0, 
           0, 0, z, 0, 
           0, 0, 0, 1);
+        break;
+      case 'matrix':
+        this.momentInverse.copy(shapeargs);
         break;
       default:
         console.log('Unimplemented inertia moment tensor: ' + shape);
@@ -255,16 +295,22 @@ elation.extend("physics.rigidbody", function(args) {
     }
   }();
   this.getContacts = function(other, collisions) {
+    var hasContacts = false;
     if (this.collider && other.collider) {
-      return this.collider.getContacts(other.collider, collisions);
+      hasContacts = this.collider.getContacts(other.collider, collisions);
     }
-    return false;
+    return hasContacts;
   }
   this.setCollider = function(type, colliderargs) {
-    if (typeof elation.physics.colliders[type] == 'function') {
-      this.collider = new elation.physics.colliders[type](this, colliderargs);
+    if (typeof type == 'object') {
+      this.collider = type;
+      this.collider.body = this;
     } else {
-      console.log('Unknown collider type ' + type);
+      if (typeof elation.physics.colliders[type] == 'function') {
+        this.collider = new elation.physics.colliders[type](this, colliderargs);
+      } else {
+        console.log('Unknown collider type ' + type);
+      }
     }
     this.collider.getInertialMoment();
   }
@@ -290,6 +336,13 @@ elation.extend("physics.rigidbody", function(args) {
       this.children.splice(idx,1);
       body.parent = undefined;
     }
+  }
+  this.processConstraints = function(contactlist) {
+    var wasConstrained = false;
+    for (var i = 0; i < this.constraints.length; i++) {
+      wasConstrained = wasConstrained || this.constraints[i].apply(contactlist);
+    }
+    return wasConstrained;
   }
   this.init();
 });
